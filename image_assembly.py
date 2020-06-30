30864c15ccfab5e6da22b1ba015794171398a002
Ryan Echols <rkechols21@gmail.com>
2020-06-30 16:58:19 -0600

    created unit test for scramble image; added function to show tiled patches

--- image_assembly.py
+++ image_assembly.py
@@ -34,6 +34,27 @@ def show_image(image_rgb: np.ndarray):
 	plt.show()
 
 
+def show_patches(patches: list, num_cols: int):
+	n = len(patches)
+	if n == 0:
+		print("no patches to show")
+		return
+	patch_size = patches[0].shape[0]  # assumes all are the same size and square
+	num_rows = n // num_cols
+	if n % num_cols != 0:
+		num_rows += 1
+	# tile the patches
+	to_show = np.zeros((num_rows * patch_size, num_cols * patch_size, 3), dtype=int)
+	for i, patch in enumerate(patches):
+		row = i // num_cols
+		col = i % num_cols
+		start_pixel_row = row * patch_size
+		start_pixel_col = col * patch_size
+		to_show[start_pixel_row:(start_pixel_row + patch_size), start_pixel_col:(start_pixel_col + patch_size), :] = patch
+	# show the tiled image
+	show_image(to_show)
+
+
 def rotate_random(patch: np.ndarray) -> (np.ndarray, int):
 	"""
 	takes an image patch and returns it at a random rotation
